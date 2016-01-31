05fe70bd2defff537a41281fd2a241758d34bd54
David Viktora <viktora.david@gmail.com>
2016-01-31 22:54:00 +0100

    Use C3 chart library. Add chart with basic stats.

--- web/myscript.js
+++ web/myscript.js
@@ -1,55 +1,76 @@
-// Load the Visualization API and the piechart package.
-google.load('visualization', '1.0', {'packages':['corechart']});
-
-// Set a callback to run when the Google Visualization API is loaded.
-google.setOnLoadCallback(drawChart);
-
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-
 var APIurl = 'http://twitter-wish-api.herokuapp.com/';
 
+// create statsChart
+var totalTweets = ['Total tweets']
+var englishTweets = ['English tweets']
+var wishes = ['Wishes']
+var times = ['times']
+var statsChart = c3.generate({
+    bindto: '#statsChart',
+    data: {
+      x: 'times',
+      xFormat: '%Y-%m-%d %H:%M:%S',
+      columns: [
+        totalTweets,
+        englishTweets,
+        wishes,
+        times
+      ]
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                format: '%H:%M:%S'
+            }
+        }
+    }
+});
+
 getLastWishes();
+getStats();
 
 window.setInterval(function(){
   getLastWishes();
-}, 1000);
+  getStats();
+}, 3000);
+
 
 function getLastWishes() {
   $.getJSON( APIurl + 'last_wishes', function(data)
     {
       var tweets = data.wishes;
-      // console.log(tweets);
       for (i = 0; i < 6; i++) {
-        document.getElementById('last_tweet_' + i).innerHTML = "<strong>" + tweets[i].username + ": </strong>" + tweets[i].tweet;
+        document.getElementById('last_tweet_' + i).innerHTML = "<strong>" +
+          tweets[i].username + ": </strong>" + tweets[i].tweet;
       }
     }
   );
 }
 
-function drawChart() {
-  // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Topping');
-  data.addColumn('number', 'Slices');
-  data.addRows([
-    ['Mushrooms', 3],
-    ['Onions', 1],
-    ['Olives', 1],
-    ['Zucchini', 1],
-    ['Pepperoni', 2]
-  ]);
 
-  // Set chart options
-  var options = {'title':'How Much Pizza I Ate Last Night',
-                 'titleTextStyle': {color: 'black', fontName: 'Arial', fontSize: '18', fontWidth: 'normal'},
-                 'width':700,
-                 'height':550,
-                 'legend':'top',
-                 'forceIFrame':'false'};
-
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
+function getStats() {
+  // get data from API
+  $.getJSON(APIurl + 'stats', function(data){
+    var stats = data.stats;
+    totalTweets = ['Total tweets'];
+    englishTweets = ['English tweets'];
+    wishes = ['Wishes'];
+    times = ['times'];
+    for (i = 0; i < stats.length; i++){
+      totalTweets.push(stats[i].tweets_total);
+      englishTweets.push(stats[i].tweets_english);
+      wishes.push(stats[i].wishes_total);
+      times.push(stats[i].datetime);
+    }
+  });
+  statsChart.load({
+    columns: [
+      totalTweets,
+      englishTweets,
+      wishes,
+      times,
+    ],
+    type: 'line'
+  });
 }
